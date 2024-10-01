1546feac8891e729bad49ba65cead3fbf27ae0a6
Ajay Dhangar <99037494+ajay-dhangar@users.noreply.github.com>
2024-10-01 19:06:14 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs">
+            to="/docs/intro/">
             Get Started DSA journey
           </Link>
         </div>
