56c0a07f8fdb025e45f41f71d66c4b29f206f418
Anuj Saha <153378181+AnujSaha0111@users.noreply.github.com>
2024-10-08 00:26:14 +0530

    Stack and Queue Data Structure

---
+++ Stacks.java
@@ -0,0 +1,74 @@
+//import java.util.*;
+//public class Stacks {
+//    public static void display(Stack<Integer> s) {
+//
+//    }
+//    public static void main(String[] args) {
+////        Scanner sc = new Scanner(System.in);
+//        Stack<Integer> st = new Stack<>();
+//        st.push(1);
+//        st.push(2);
+//        st.push(3);
+//        st.push(4);
+//        st.push(5);
+//        System.out.println();
+//        st.pop();
+//        System.out.println(st);
+////        int n = st.size();
+////        int[] arr = new int[n];
+////        for (int i = n - 1; i >= 0; i--) {
+////            arr[i] = st.pop();
+////        }
+////        for (int i = 0; i < n; i++) {
+////            System.out.print(arr[i] + " ");
+////            st.push(arr[i]);
+////        }
+////        Stack<Integer> rt = new Stack<>();
+////        while(st.size()>0) {
+////            rt.push(st.pop());
+////        }
+////        while(rt.size()>0) {
+////            int x = rt.pop();
+////            System.out.print(x + " ");
+////            st.push(x);
+////        }
+////        System.out.println(st);
+////        int n;
+////        System.out.println("Enter the number of elements : - ");
+////        n = sc.nextInt();
+////        System.out.println("Enter the elements : ");
+////        for (int i = 1; i <= n; i++) {
+////            int x = sc.nextInt();
+////            st.push(x);
+////        }
+////        System.out.println(st);
+////        int idx = 2;
+////        int x = 7;
+////        Stack<Integer> temp = new Stack<>();
+////        while(st.size() > idx) {
+////            temp.push(st.pop());
+////        }
+////        st.push(x);
+////        while (temp.size() > 0) {
+////            st.push(temp.pop());
+////        }
+////        System.out.println(st);
+////            rt.push(x);
+////            st.pop();
+////        }
+////        System.out.println(rt);
+////        st.push(1);
+////        st.push(15);
+////        st.push(25);
+////        st.push(50);
+////        System.out.println(st.peek());
+////        System.out.println(st);
+////        st.pop();
+////        System.out.println(st);
+////        System.out.println("Size is : " + st.size());
+////        while (st.size()>2) {
+////            st.pop();
+////        }
+////        System.out.println(st.peek());
+//    }
+//}
