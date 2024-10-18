0ee58fa48d01246d32c8fddefe70bc5c18773fb2
karanmaheshwari16 <karan.maheshwari167@gmail.com>
2024-10-18 09:45:25 +0530

    Trees Algorithms Updated

--- trees/bst/4. Replacing.cpp
+++
@@ -1,5 +0,0 @@
-//There is no Direct Way to replace elements in BST because
-//elements are stored in a specific manner and replacing a element directly might disturb the property of BST
-
-//TO REPLACE ELEMENTS IN A BST,
-//FIRST DELETE REPLACED ELEMENT THEN INSERT REPLACING ELEMENT
