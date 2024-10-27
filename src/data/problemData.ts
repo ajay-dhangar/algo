4d9fcc9c8944e8b73ed34aa9148e36472bd21feb
SrijaVuppala295 <srijavuppala295@gmail.com>
2024-10-27 22:34:25 +0530

    done

--- src/data/problemData.ts
+++ src/data/problemData.ts
@@ -2159,6 +2159,17 @@ class Solution:
           return n * (n + 1) // 2 - sum(nums)
       `,
     },
+    timeComplexity: {
+        cpp: "O(n)",
+        java: "O(n)",
+        python: "O(n)",
+    },
+    spaceComplexity: {
+        cpp: "O(1)",
+        java: "O(1)",
+        python: "O(1)",
+    },
+    
   },
 
   guessNumber: {
