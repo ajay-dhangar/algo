81a9326b00468583f5e2ad15ed42229e80ec157d
yashksaini-coder <ys3853428@gmail.com>
2024-10-30 10:29:39 +0530

    refactor: 💡 Change a --> Link && href --> to

--- src/components/Homepage/HeroSection.tsx
+++ src/components/Homepage/HeroSection.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FaGithub, FaArrowRight } from "react-icons/fa";
 import AnimatedGIF from "./AnimatedGIF";
 import mainGif from "./assets/main.gif";
+import { Link } from "@docusaurus/Link";
 
 
 const HeroSection = () => {
@@ -16,18 +17,18 @@ const HeroSection = () => {
           understanding from the basics to advanced topics.
         </p>
         <div className="flex flex-col md:flex-row items-center justify-center space-y-4 md:space-y-0 md:space-x-6">
-          <a
-            href="/docs"
+          <Link
+            to="/docs"
             className="flex items-center px-6 py-3 text-lg font-medium text-white bg-blue-600 hover:bg-blue-700 rounded-lg transition duration-300 ease-in-out shadow-lg transform hover:scale-105"
           >
             Explore Algorithms <FaArrowRight className="ml-2" />
-          </a>
-          <a
-            href="https://github.com/ajay-dhangar/algo"
+          </Link>
+          <Link
+            to="https://github.com/ajay-dhangar/algo"
             className="flex items-center px-6 py-3 text-lg font-medium text-blue-600 dark:text-white border-2 border-blue-600 dark:border-white rounded-lg transition duration-300 ease-in-out hover:bg-blue-600 hover:text-white dark:hover:bg-white dark:hover:text-blue-600"
           >
             View on GitHub <FaGithub className="ml-2" />
-          </a>
+          </Link>
         </div>
         <br />
         <div className="mt-12">
