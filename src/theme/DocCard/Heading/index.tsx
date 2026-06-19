d2a718772a06a9439f64451e9386067d41b75dd0
Ajay Dhangar <99037494+ajay-dhangar@users.noreply.github.com>
2026-06-19 09:42:51 +0530

    Refactor DocCardHeading component structure

--- src/theme/DocCard/Heading/index.tsx
+++ src/theme/DocCard/Heading/index.tsx
@@ -1,21 +1,28 @@
-import React, {type ReactNode} from 'react';
+import React, { type ReactNode } from 'react';
 import clsx from 'clsx';
-import {ThemeClassNames} from '@docusaurus/theme-common';
-import Heading from '@theme/Heading';
+import { ThemeClassNames } from '@docusaurus/theme-common';
 import Icon from '@theme/DocCard/Heading/Icon';
 import Text from '@theme/DocCard/Heading/Text';
-import type {Props} from '@theme/DocCard/Heading';
+import type { Props } from '@theme/DocCard/Heading';
 
 import styles from './styles.module.css';
 
-export default function DocCardHeading({item, title, icon}: Props): ReactNode {
+export default function DocCardHeading({ item, title, icon }: Props): ReactNode {
   return (
-    <Heading
-      as="h2"
-      className={clsx(ThemeClassNames.docs.docCard.heading, styles.cardTitle)}
-      title={title}>
-      {icon && <Icon item={item} icon={icon} />}
-      <Text item={item} title={title} />
-    </Heading>
+    <div
+      className={clsx(
+        ThemeClassNames.docs.docCard.heading, 
+        styles.cardHeaderContainer
+      )}
+    >
+      {icon && (
+        <div className={styles.iconWrapper}>
+          <Icon item={item} icon={icon} />
+        </div>
+      )}
+      <div className={styles.textWrapper}>
+        <Text item={item} title={title} />
+      </div>
+    </div>
   );
 }
