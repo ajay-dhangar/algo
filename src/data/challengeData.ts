1089fb99c4ef3dd4713dde946de5f346c90a810a
KANISHKA GUPTA <kanishkagupta2607@gmail.com>
2026-07-18 16:19:30 +0530

    challengediff

--- src/data/challengeData.ts
+++ src/data/challengeData.ts
@@ -1,26 +1,5 @@
 const challengeData = [
   {
-    title: "Challenge 1: Data Structures-1",
-    description:
-      "Solve the problem within 30 minutes to earn points and rank up.",
-    timeLimit: "30 min",
-    link: "/challenges/challenge1",
-  },
-  {
-    title: "Challenge 2: Data Structures-2",
-    description:
-      "Solve this challenging problem within 30 minutes to earn even more points!",
-    timeLimit: "30 min",
-    link: "/challenges/challenge2",
-  },
-  {
-    title: "Challenge 3: Advanced Data Structures",
-    description:
-      "Solve this challenging problem within 60 minutes to earn even more points!",
-    timeLimit: "60 min",
-    link: "/challenges/challenge3",
-  },
- {
     title: "Tree Traversals (Inorder, Preorder, Postorder)",
     description:
       "Return all three traversals of a binary tree. Great entry point into recursive tree thinking.",
