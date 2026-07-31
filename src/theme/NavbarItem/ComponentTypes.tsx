62f3ea53e132224c1aa92a3e3f1df718c4dec079
KANISHKA GUPTA <kanishkagupta2607@gmail.com>
2026-07-31 09:35:37 +0530

    theme options

---
+++ src/theme/NavbarItem/ComponentTypes.tsx
@@ -0,0 +1,7 @@
+import ComponentTypes from '@theme-original/NavbarItem/ComponentTypes';
+import ThemePicker from '../../components/ThemePicker';
+
+export default {
+  ...ComponentTypes,
+  'custom-themePicker': ThemePicker,
+};
