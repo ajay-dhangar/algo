d5664f24eaa8b699288f76892b05ff7b8a0f4f44
Kanishka Gupta <kanishkagupta2607@gmail.com>
2026-08-10 18:39:15 +0530

    Delete show_line.py

--- show_line.py
+++
@@ -1,8 +0,0 @@
-﻿from pathlib import Path
-path = Path('src/components/ComingSoon.tsx')
-text = path.read_text(encoding='utf8')
-line_num = 158
-lines = text.splitlines()
-line = lines[line_num-1]
-print(line_num, line)
-print('chars:', [(i+1, ch) for i,ch in enumerate(line)])
