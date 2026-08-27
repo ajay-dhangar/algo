ede6529e17d57cc4a9f42a826877653a13df5c1b
Ajay Dhangar <99037494+ajay-dhangar@users.noreply.github.com>
2026-08-27 08:48:09 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,15 +6,13 @@ import Homepage from "../components/Homepage";
 import BottomToTop from "../components/Scroller/BottomToTop/BottomToTop.tsx";
 import TopToBottom from "../components/Scroller/TopToBottom/TopToBottom.tsx";
 
-
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
-
-
+  
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title={`DSA Guide | ${siteConfig.title}`}
+      description="Learn data structures and algorithms with our comprehensive, open-source educational platform. Explore clean solutions, boost your coding interview prep, and contribute to world-class developer documentation."
     >
       <Head>
         <script
@@ -22,10 +20,27 @@ export default function Home() {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5832817025080991"
           crossorigin="anonymous"
         />
-        <meta name='impact-site-verification' value='1e9bf198-a4f7-4132-b77d-46b34e45f6ad' />
+        <meta name='impact-site-verification' value='1e9bf198-a4f7-4132-b77d-46b34e45f6ad' />   
+         {/* Inline Script for AdSense Initialization */}
+        <script>
+          {`
+            (adsbygoogle = window.adsbygoogle || []).push({});
+          `}
+        </script>
       </Head>
       <main>
         <Homepage />
+        {/* AdSense In-feed / Display Unit */}
+        <div style={{ padding: '10px 0' }}>
+          <ins 
+            className="adsbygoogle"
+            style={{ display: 'block' }}
+            data-ad-format="fluid"
+            data-ad-layout-key="-fd-24+9-a1+se"
+            data-ad-client="ca-pub-5832817025080991"
+            data-ad-slot="4344560339"
+          />
+        </div>
         <BottomToTop />
         <TopToBottom />
       </main>
